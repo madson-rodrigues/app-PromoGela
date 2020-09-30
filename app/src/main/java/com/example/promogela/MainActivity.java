6baf53263e99f90e515b926e035a801ccd0a1b5e
Madson <liborio.madson@gmail.com>
2020-09-30 14:42:41 -0300

    first commit!

---
+++ app/src/main/java/com/example/promogela/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.promogela;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Feed");
+        arrayList.add("Mapa");
+        arrayList.add("Perfil");
+
+        prepareViewPager(viewPager, arrayList);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_feed);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_mapa);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_perfil);
+
+        tabLayout.getTabAt(0).getIcon().setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+        tabLayout.getTabAt(1).getIcon().setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+        tabLayout.getTabAt(2).getIcon().setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                for(int i=0; i<tabLayout.getTabCount(); i++) {
+                    if(tabLayout.getTabAt(i).isSelected())
+                        tabLayout.getTabAt(i).getIcon().setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+                    else{
+                        tabLayout.getTabAt(i).getIcon().setColorFilter(getResources().getColor(android.R.color.darker_gray), PorterDuff.Mode.SRC_IN);
+                    }
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+    }
+
+
+    private void prepareViewPager(ViewPager viewPager, ArrayList<String> arrayList) {
+        MainAdapter adapter = new MainAdapter(getSupportFragmentManager());
+        // inicializar main fragment
+        MainFragment fragment = new MainFragment();
+
+        for(int i=0; i<arrayList.size(); i++){
+            //iniciar bundle
+            Bundle bundle = new Bundle();
+            //colocar string
+            bundle.putString("title", arrayList.get(i));
+            //setar argumentos
+            fragment.setArguments(bundle);
+            //adicionar fragmento
+            adapter.addFragment(fragment, arrayList.get(i));
+            //definir novo fragmento
+            fragment = new MainFragment();
+        }
+        //setar adapter
+        viewPager.setAdapter(adapter);
+
+    }
+
+    private class MainAdapter extends FragmentPagerAdapter {
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        List<Fragment> fragmentList = new ArrayList<>();
+
+        public void addFragment (Fragment fragment,String title){
+            arrayList.add(title);
+            fragmentList.add(fragment);
+        }
+
+        public MainAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return arrayList.get(position);
+
+        }
+    }
+}
